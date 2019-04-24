86c7be8f76bba8a8ac77c88b0ec0eda47f1a1643
emanmacario <emacario1997@hotmail.com>
2019-04-24 17:43:02 +1000

    Obtain Levenshtein results

--- approx-string-search.py
+++ approx-string-search.py
@@ -6,76 +6,105 @@ Student Number: 831659
 # Import useful libraries
 from ctypes import *
 from collections import defaultdict
+import math
+import nltk
+import json
 
 
 # Cython SENTDEX Tutorial
 # https://www.youtube.com/watch?v=mXuEoqK4bEc
 
-# WORDSname constants
+# Filename constants
 MISSPELL_WORDS = 'misspell.txt'
 DICT_WORDS = 'dict.txt'
 CORRECT_WORDS = 'correct.txt'
 
 
-"""
-TODO: Process each unique s
-"""
+def main2():
+    with open('levenshtein.json') as f:
+        results = json.load(f)
+        print(results['thirsty'])
 
 
 def main():
-    # Store dictionary words as a set
-    dictionary = set()
-    with open(DICT_WORDS) as f:
-        for word in f:
-            word = word.strip()
-            dictionary.add(word)
-
-    # Get unique misspelled tokens
-    unique_misspelled = set()
-    with open(MISSPELL_WORDS) as f:
-        for word in f:
-            word = word.strip()
-            unique_misspelled.add(word)
+    dictionary = get_dictionary(DICT_WORDS)
+    unique_misspelled = get_unique_misspelled(MISSPELL_WORDS)
 
     # Extract the 'gold standard', storing all of the 'correct'
     # words each unique 'misspelled' word can be corrected to
     with open(MISSPELL_WORDS) as misspell, open(CORRECT_WORDS) as correct:
-        gold_standard = defaultdict(set)
+        gold_standard = []
         for m, c in zip(misspell, correct):
             m = m.strip()
             c = c.strip()
-            gold_standard[m].add(c)
-    print(len(gold_standard))
-
-    # DEBUGGING
-    """
-    for m in gold_standard:
-        if len(gold_standard[m]) > 1:
-            print("{:12s}:".format(m) + ' ' + str(gold_standard[m]))
-    """
+            gold_standard.append((m, c))
 
     # Load the shared object file
     edit_distance = CDLL('./edit_distance.so')
 
-    # Create byte objects from the strings
-    b_m = "gily".encode('utf-8')
-    b_d = "geely".encode('utf-8')
-
-
-
-    m1 = "honda"
-    d1 = "hyundai"
-
-    r = edit_distance.levenshtein(m1.encode('utf-8'), d1.encode('utf-8'));
-    print(r)
-
-    m = "julie".encode('utf-8')
+    # For every unique misspelled word, get the best match(es)
+    # according to the specific edit distance function
+    results = {}
     total = 0
-    for word in dictionary:
-        d = word.encode('utf-8')
-        r = edit_distance.levenshtein(m, d)
+    LIMIT = 10
+    for misspelled in sorted(unique_misspelled):
+
+        # Debugging
+        # if total >= LIMIT:
+        #    break
+
+        matches = []
+        lowest = math.inf
+        results[misspelled] = {'distance': lowest, 'matches': matches}
+
+        # Convert strings to byte objects
+        b_misspelled = misspelled.encode('utf-8')
+
+        for word in dictionary:
+            b_word = word.encode('utf-8')
+
+            # Calculate Levenshtein distance
+            distance = edit_distance.levenshtein(b_misspelled, b_word)
+
+            if distance == lowest:
+                matches.append(word)
+            elif distance < lowest:
+                lowest = distance
+                results[misspelled]['distance'] = lowest
+                matches.clear()
+                matches.append(word)
         total += 1
-    print("Total dict words:", total)
+
+        if total % 75 == 0:
+            print("Total of {:4d}/{:4d} unique misspelled words processed".format(total, len(unique_misspelled)))
+
+    # Store results in a JSON file
+    with open('levenshtein.json', 'x') as fp:
+        json.dump(results, fp, indent=4)
+
+
+def get_dictionary(file):
+    # Store dictionary words as a set
+    dictionary = set()
+    with open(file) as f:
+        for word in f:
+            word = word.strip()
+            dictionary.add(word)
+    return dictionary
+
+
+def get_unique_misspelled(file):
+    # Get unique misspelled tokens
+    unique_misspelled = set()
+    with open(file) as f:
+        for word in f:
+            word = word.strip()
+            unique_misspelled.add(word)
+    return unique_misspelled
+
+
+if __name__ == "__main__":
+    main()
 
 
 def print_interesting():
@@ -153,5 +182,3 @@ def print_stats():
         print("Total unique correct words:", len(unique_correct_words))
 
 
-if __name__ == "__main__":
-    main()
